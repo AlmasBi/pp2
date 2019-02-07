8e2373abcc1954ce585e3c1716347b282a35dc69
AlmasBi <47265704+AlmasBi@users.noreply.github.com>
2019-02-07 23:16:25 +0600

    Add files via upload

---
+++ week1/1 L 3 E/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_L_3_E
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            int l = 0;
+            a = Console.ReadLine();
+            int c = int.Parse(a);
+            string[] arr = Console.ReadLine().Split();
+            for (int i = 0; i < c; i++)
+            {
+                
+                    int q = int.Parse(arr[i]);
+                Console.Write(q + " ");
+                Console.Write(q + " ");
+
+            } Console.ReadKey();
+        }
+    }
+}
