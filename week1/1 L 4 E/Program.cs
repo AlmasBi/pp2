6f164f5ee337887b1c587b3e17e8e32e4031be17
AlmasBi <47265704+AlmasBi@users.noreply.github.com>
2019-02-07 23:17:02 +0600

    Add files via upload

---
+++ week1/1 L 4 E/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_L_4_E
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tring s = "[*]";  //  один экземпляр star triangle 
+            int a = int.Parse(Console.ReadLine());  //  Вводим число
+            for (int i = 1; i <= a; i++)  //      используем loop в loop-е
+            {                               //  
+                for (int j = 1; j <= i; j++)  //  
+                {
+                    Console.Write(s);  // выводим возрастающее каждой итерацией startriangle
+                }
+                Console.WriteLine();   //  переход в следующую строку
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
