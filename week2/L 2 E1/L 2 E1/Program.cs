1b19bde86c3fedc83ef9ea0fce0612194f021cda
AlmasBi <47265704+AlmasBi@users.noreply.github.com>
2019-02-07 23:21:02 +0600

    Add files via upload

---
+++ week2/L 2 E1/L 2 E1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace L_2_E1
+{
+   
+
+    class Program
+    {
+        public static bool Ex7()
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            String s = sr.ReadToEnd();
+            String[] arr = s.Split();
+            bool qw = false;
+            for (int i = 0; i <= arr.Length; i++)
+            {
+                if (arr[0] == arr[arr.Length - 1 - i])
+                {
+                    qw = true;
+                    return qw;
+                }
+
+            }return qw;
+        }
+
+        static void Main(string[] args)
+        {
+         
+               if(Ex7())
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+            Console.ReadKey();
+        }
+    }
+}
