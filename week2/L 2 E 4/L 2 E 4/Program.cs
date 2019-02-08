c277b3622253ef4a07ef8beebfe3b616345e315f
AlmasBi <47265704+AlmasBi@users.noreply.github.com>
2019-02-08 23:57:27 +0600

    Add files via upload

---
+++ week2/L 2 E 4/L 2 E 4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace L_2_E_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string q = @"C:\Users\Lenovo\Desktop\C#\L 2 E 4\L 2 E 4\text1.txt";    //  Пути для создания 
+            string w = @"C:\Users\Lenovo\Desktop\C#\L 2 E 4\L 2 E 4\text2.txt";    //     файлов
+            string asd = "KBTU is the best!!!";
+            StreamWriter dd = new StreamWriter(q);
+            dd.Write(asd);
+            dd.Close();
+            File.Copy(q, w);   //  Копируем  в новый файл
+            File.Delete(q);   //  Удаляем первый файл
+        }
+    }
+}
