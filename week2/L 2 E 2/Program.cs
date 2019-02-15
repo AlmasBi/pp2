accdd303c13f2b8bfc7afeba4519b86599fca89c
AlmasBi <47265704+AlmasBi@users.noreply.github.com>
2019-02-15 21:19:52 +0600

    Update Program.cs

--- week2/L 2 E 2/Program.cs
+++ week2/L 2 E 2/Program.cs
@@ -9,36 +9,29 @@ namespace L_2_E_2
 {
     class Program
     {
-        public static void Ex2(int t)
-        {
-            
-            
-           
-
-        }
         public static void Ex1()
         {
-            StreamWriter sw = new StreamWriter(@"C:\Users\Lenovo\Desktop\C#\L 2 E 2\L 2 E 2\bin\Debug\output.txt");
-            StreamReader sr = new StreamReader(@"C:\Users\Lenovo\Desktop\C#\L 2 E 2\L 2 E 2\bin\Debug\input.txt");
-            string s = sr.ReadToEnd();
-            string[] arr = s.Split();
+            StreamWriter sw = new StreamWriter(@"C:\Users\Lenovo\Desktop\C#\L 2 E 2\L 2 E 2\bin\Debug\output.txt");// "инструмент" для ввода текста
+            StreamReader sr = new StreamReader(@"C:\Users\Lenovo\Desktop\C#\L 2 E 2\L 2 E 2\bin\Debug\input.txt");// "инструмент" для чтения текста
+            string s = sr.ReadToEnd(); //  переменная  s принимает значение текста 
+            string[] arr = s.Split(); // разделяем текст на элементы стрингового массива через пробел
             for(int i = 0; i < arr.Length; i++)
             {
                 int n = 0;
-                int q = int.Parse(arr[i]);
-                for(int j = 2; j <=q; j++)
+                int q = int.Parse(arr[i]);// конвертируем каждый элемент массива со стринга в интеджер
+                for(int j = 2; j <=q; j++)// пробегаемся по количеству элементов массива
                 {
                     if (q % j == 0)
                         n++;
                 }
-                if (n == 1)
-                    sw.Write(q + " ");
+                if (n == 1)// проверяем простое ли число
+                    sw.Write(q + " ");// если простое, то вводим его в текст output
             } sw.Close();
             sr.Close();
         }
         static void Main(string[] args)
         {
-            Ex1();
+            Ex1();//Вызываем функцию Ex1
             Console.ReadKey();
         }
     }
