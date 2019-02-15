861b220f4f1ee6ef3ea6363608fd000e207674b9
AlmasBi <47265704+AlmasBi@users.noreply.github.com>
2019-02-15 21:24:14 +0600

    Update Program.cs

--- week1/1 L 3 E/Program.cs
+++ week1/1 L 3 E/Program.cs
@@ -12,14 +12,14 @@ namespace _1_L_3_E
         {
             string a;
             int l = 0;
-            a = Console.ReadLine();
-            int c = int.Parse(a);
-            string[] arr = Console.ReadLine().Split();
+            a = Console.ReadLine();//  количество элементов первого массива
+            int c = int.Parse(a);// конвертируем  элемент со стринга в интеджер
+            string[] arr = Console.ReadLine().Split();// разделяем каждое число массива в виде массива строк
             for (int i = 0; i < c; i++)
             {
                 
-                    int q = int.Parse(arr[i]);
-                Console.Write(q + " ");
+                    int q = int.Parse(arr[i]);// все элементы массива строк конвертируем в элементы массива целых чисел 
+                Console.Write(q + " ");//  Выводим элементы массива два раза
                 Console.Write(q + " ");
 
             } Console.ReadKey();
